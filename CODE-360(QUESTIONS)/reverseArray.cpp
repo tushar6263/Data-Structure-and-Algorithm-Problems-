4f049e17c9082d8088e7076e1b8a2763938babf7
tushar626346 <tushar626346@gmail.com>
2025-03-02 23:17:37 +0530

    add files

---
+++ CODE-360(QUESTIONS)/reverseArray.cpp
@@ -0,0 +1,16 @@
+//---->Question Link:-https://www.naukri.com/code360/problems/reverse-the-array_1262298---------------
+
+//------>Solution:-
+
+// void reverseArray(vector<int> &arr , int m) {
+//     // Write your code here
+//     int s = m+1;
+//     int e = arr.size()-1;      
+//     while(e>=s){
+//         swap(arr[s],arr[e]);
+//         s++;
+//         e--;
+
+//     } 	
+// }
+
---
+++ LEETCODE-QUESTIONS/moveZeros.cpp
@@ -0,0 +1,17 @@
+//-->Question:-https://leetcode.com/problems/move-zeroes/description/
+
+//Solution-->
+// class Solution {
+// public:
+// void moveZeroes(vector<int>& nums) {
+//     int nonZero =0;
+
+//     for(int j=0;j<nums.size();j++){
+//         if(nums[j]!=0){
+//             swap(nums[j],nums[nonZero]);
+//         nonZero++;
+//         }
+
+//     }
+// }
+// };
