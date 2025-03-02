a25bc80120dfc31e8bc3d9238ebde497b4812d0e
tushar626346 <tushar626346@gmail.com>
2025-03-02 22:43:56 +0530

    STL

---
+++ STL/algorithm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(7);
+
+    cout<<"Finding 6-->"<<binary_search(v.begin(),v.end(),6)<<endl;
+
+     return 0;
+}
