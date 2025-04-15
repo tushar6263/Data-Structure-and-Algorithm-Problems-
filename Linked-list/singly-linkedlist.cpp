95904b7448d03a3c0a143da7f5782d8a1ab8bfde
tushar626346 <tushar626346@gmail.com>
2025-04-15 21:46:46 +0530

    add new files

--- Linked-list/singly-linkedlist.cpp
+++ Linked-list/singly-linkedlist.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 class Node
@@ -14,15 +15,17 @@ public:
         this->next = NULL;
     }
 
-    //destructor
-    ~Node(){
+    // destructor
+    ~Node()
+    {
         int value = this->data;
-        //memory free
-        if(this->next!=NULL){
-               delete next;
-               this->next =NULL;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
         }
-        cout<<"memory is free for node with data"<<value<<endl;
+        cout << "memory is free for node with data" << value << endl;
     }
 };
 
@@ -57,14 +60,14 @@ void Print(Node *&head)
     cout << endl;
 }
 
-void insertAtPosition(Node* &tail ,Node *&head, int position, int d)
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
 {
-    //insert at start
-    if(position==1){
-        insertAtHead(head,d);
+    // insert at start
+    if (position == 1)
+    {
+        insertAtHead(head, d);
         return;
     }
-
 
     Node *temp = head;
     int cnt = 1;
@@ -73,8 +76,9 @@ void insertAtPosition(Node* &tail ,Node *&head, int position, int d)
         temp = temp->next;
         cnt++;
     }
-    if(temp->next == NULL){
-        insertAtTail(tail,d);
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
         return;
     }
 
@@ -84,34 +88,116 @@ void insertAtPosition(Node* &tail ,Node *&head, int position, int d)
     temp->next = nodeToInsert;
 }
 
-void deleteNode(int position,Node* &head , Node* &tail){
-    //deleting first or start node
-    if(position==1){
-        Node* temp = head;
-        head = head ->next;
-        //memory free start node
-        temp ->next =  NULL;
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    // deleting first or start node
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        // memory free start node
+        temp->next = NULL;
         delete temp;
     }
-    else{
-        //deleting any middle node or last node
-        Node* curr = head;
-        Node* prev = NULL;
+    else
+    {
+        // deleting any middle node or last node
+        Node *curr = head;
+        Node *prev = NULL;
 
-        int cnt =1;
-        while (cnt < position){
-         prev = curr;
-         curr =  curr -> next;
-         cnt++;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
         }
-        if(curr-> next == NULL){
+        if (curr->next == NULL)
+        {
             tail = prev;
         }
-     prev->next = curr ->next;
-     curr -> next = NULL;
-     delete curr;
-        
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
     }
+}
+
+bool detectLoop(Node *head)
+{
+    if (head == NULL)
+    {
+        return false;
+    }
+    map<Node *, bool> visited;
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (visited[temp] == true)
+        {
+            cout << "Present on element " << temp->data << endl;
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+Node *floydDetectLoop(Node *head)
+{
+
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    Node *slow = head;
+    Node *fast = head;
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+
+        slow = slow->next;
+        if (slow == fast)
+        {
+            cout<<"persent at "<<slow ->data<<endl;
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+Node* getstartingNode(Node* head){
+    if(head == NULL){
+        return NULL;
+    }
+    Node* slow = head;
+    Node* intersection = floydDetectLoop(head);
+
+    while (slow != intersection)
+    {
+        slow = slow ->next;
+        intersection = intersection->next;
+    }
+    return  slow;
+}
+
+Node* removeLoop(Node* head){
+    if(head ==NULL){
+        return NULL;
+    }
+    Node* startodLoop = getstartingNode(head);
+    Node* temp = startodLoop;
+    while (temp ->next != startodLoop)
+    {
+        temp = temp -> next;
+    }
+    temp -> next = NULL;
+    
 }
 
 int main()
@@ -124,24 +210,48 @@ int main()
     Node *tail = node1;
 
     // Print initial linked list
-    Print(head);
+    //   Print(head);
 
     // Insert nodes at the tail
     insertAtTail(tail, 12);
-    Print(head);
+    //  Print(head);
 
     insertAtTail(tail, 15);
+    //  Print(head);
+
+    insertAtPosition(tail, head, 4, 22);
+    // Print(head);
+
+    // cout<<"Head " <<head->data<<endl;
+    // cout<<"Tail "<<tail->data<<endl;
+
+    // deleteNode(4,head,tail);
+    tail->next = head->next;
+    // Print(head);
+    cout << "Head " << head->data << endl;
+    cout << "Tail " << tail->data << endl;
+    // Print(head);
+
+    // if (detectLoop(head))
+    // {
+    //     cout << "Cycle is persent" << endl;
+    // }
+    // else
+    // {
+    //     cout << "cycle is not persent" << endl;
+    // }
+    if (floydDetectLoop(head) != NULL)
+    {
+        cout << "Cycle is persent" << endl;
+    }
+    else
+    {
+        cout << "cycle is not persent" << endl;
+    }
+
+    cout<<"Loop starting at "<<getstartingNode(head)->data<<endl;
+    removeLoop(head);
     Print(head);
 
-    insertAtPosition(tail,head,4,22);
-    Print(head);
-
-    cout<<"Head " <<head->data<<endl;
-    cout<<"Tail "<<tail->data<<endl;
-
-    deleteNode(4,head,tail);
-    Print(head);
-    cout<<"Head " <<head->data<<endl;
-    cout<<"Tail "<<tail->data<<endl;
     return 0;
 }
