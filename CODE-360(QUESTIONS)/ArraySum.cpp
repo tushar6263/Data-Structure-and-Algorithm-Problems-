7996d57f9649cba2a5a8fb2dff245d7723ee07e8
tushar626346 <tushar626346@gmail.com>
2025-04-20 12:17:06 +0530

    update the css

---
+++ CODE-360(QUESTIONS)/ArraySum.cpp
@@ -0,0 +1,11 @@
+//Question Link:-https://www.naukri.com/code360/guided-paths/data-structures-algorithms/content/607713/offering/9536704
+
+//Solutionn:-
+// int arraySum(int arr[], int n)
+// {
+//    int sum =0;
+//    for(int i=0 ;i<n;i++){
+//        sum = sum + arr[i];
+//    }
+//    return sum;
+// }
---
+++ CODE-360(QUESTIONS)/LargestInArray.cpp
@@ -0,0 +1,13 @@
+//Question Link:-https://www.naukri.com/code360/guided-paths/data-structures-algorithms/content/607713/offering/9536705
+
+//Solution:-
+// #include <bits/stdc++.h> 
+// int largestElement(vector<int> &arr, int n) {
+//     int LargeNum = arr[0];
+//     for(int i=0 ;i<n;i++){
+//         if(arr[i]>=LargeNum){
+//             LargeNum = arr[i];
+//         }
+//     }
+//         return LargeNum;
+// }
