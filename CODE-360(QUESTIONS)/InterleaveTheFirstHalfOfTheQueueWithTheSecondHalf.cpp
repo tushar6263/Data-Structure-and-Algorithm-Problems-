2b97b81bef27228b592cac158bccc3d231fc5da3
tushar626346 <tushar626346@gmail.com>
2025-05-03 20:12:29 +0530

    add new files

---
+++ CODE-360(QUESTIONS)/InterleaveTheFirstHalfOfTheQueueWithTheSecondHalf.cpp
@@ -0,0 +1,26 @@
+//Question Link:-https://www.naukri.com/code360/problems/interleave-the-first-half-of-the-queue-with-the-second-half_1169450
+
+//Solution:-
+// #include <bits/stdc++.h> 
+// void interLeaveQueue(queue < int > & q) {
+//      queue<int> newq;
+//     int halfSize = q.size() / 2;
+
+//     // Push first half of the queue into newq
+//     for (int i = 0; i < halfSize; i++) {
+//         int val = q.front();
+//         newq.push(val);
+//         q.pop();
+//     }
+
+//     // Interleave elements from newq and remaining elements in q
+
+//     while(!newq.empty()){
+//        int val = newq.front();
+//        newq.pop();
+//        q.push(val);
+//        val = q.front();
+//        q.pop();
+//        q.push(val);
+//     }
+// }
